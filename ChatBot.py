4a04afbd9b6a1a1777a44b42095485536f8bfc49
Tim Eichinger <business.timeichinger@gmail.com>
2019-04-04 14:30:52 +0200

    Modifies

--- ChatBot.py
+++ ChatBot.py
@@ -5,7 +5,6 @@
 
 from selenium import webdriver
 import time
-import random
 
 # get driver and open web-page
 driver = webdriver.Chrome()
@@ -14,30 +13,106 @@ driver.get('https://web.whatsapp.com/')
 # variable for the length before scanning
 global length_before
 
+# variable that saves whether the bot has answered this person or group before
+global already_answered
+
+# Bot Answers and helping variables
+self_online = False
+
+# groups and chats where the bot should not answer
+groups_without_authotization = ['Ersti WiWi', 'Uni Elite und Sascha', 'A-Jugend 2018/2019',
+                                'Renate Mehringer', 'WInfo Erstis WS 18/19', ' Spieler SV 22',
+                                'Schischule B.-A.-Z. ', 'Silke Eichinger']
+
+# fortnite keywords and answer when they are detected
+fortnite_words = ['F?', 'f', 'F', 'f?', 'fortnite', 'Fortnite', 'Fortnite?', 'fortnite?']
+fortnite_answer = 'Tim\'s Freizeit ist heute von 10.30 Uhr bis 11.45 Uhr ' \
+                  'und von 12.15 Uhr bis 14.30. '
+
+# discord keywords and answer when they are detected
+discord_words = ['disc', 'Disc', 'disc?', 'Disc?', 'Discord', 'discord', 'Discord?',
+                 'discord?', 'on', 'on?', 'On', 'online', 'Online' ]
+discord_answer = 'Tim ist den ganzen Tag über Discord erreichbar. '
+
+# bad or offensive words that the bot can detect
 bad_words = ['Larry', 'larry', 'Spast', 'spast', 'Spasti', 'spasti', 'Depp', 'depp', 'hurensohn',
-             'Hurensohn', 'Huansohn', 'huansohn', 'wixer', 'Wixer']
+             'Hurensohn', 'Huansohn', 'huansohn', 'wixer', 'Wixer', 'Sau', 'sau', 'Arsch', 'arsch', 'Arschloch',
+             'arschloch']
 
 
-def get_answer(texts):
+# machine learning answer
+def get_machine_algorithm_answer():
+    return ''
+
+
+# returns the answer the chat bot should print
+def get_answer(texts, chat_name):
     list_splitted_msg = str(texts[-1]).split()
+
+    return_string = ""
+
+    discord_already_in_string = False
+    fortnite_already_in_string = False
+    bad_words_already_in_string = False
+
+    if 'Tim Eichinger' in str(texts[-1]) or 'Tim' in str(texts[-1]):
+        if self_online:
+            return_string += get_machine_algorithm_answer()
+        else:
+            return_string += 'Tim ist zurzeit nicht online. Er antwortet dir/euch innerhalb 20min! '
+    elif chat_name not in already_answered:
+        return_string += 'Tim ist zurzeit nicht online.'
+        already_answered.append(chat_name)
+
     for item in list_splitted_msg:
-        if item in bad_words:
-            i = random.randint(0, len(bad_words) - 1)
-            return 'Ich habe eine Beleidigung entdeckt ({})! Du {}!'.format(item, bad_words[i])
-    for message in texts:
-        if 'Tim Eichinger' in str(message):
-            return 'Tim ist zurzeit nicht online.'
+        if item in bad_words and not bad_words_already_in_string:
+            return_string += 'Ich habe eine Beleidigung entdeckt (\"{}\")! Das kann ich nicht tolerieren! '.format(item)
+            bad_words_already_in_string = True
+        if item in fortnite_words and not fortnite_already_in_string:
+            return_string += fortnite_answer
+            fortnite_already_in_string = True
+        if item in discord_words and not discord_already_in_string:
+            return_string += discord_answer
+            discord_already_in_string = True
+
+    return return_string
+
+# text dir class: _2S1VP copyable-text selectable-text
+# button class: _1aTxu
 
 
+# Checks, whether the bot can send a message to the specific chat and handles the following send process
 def send_text(par_elements, par_msg_box):
+    go_to_infos = driver.find_element_by_class_name('_5SiUq')
+    go_to_infos.click()
+
+    time.sleep(3)
+
+    chat_name = driver.find_element_by_xpath('//div[@class = "{}"]'.format('_2S1VP copyable-text selectable-text')).text
+
+    if chat_name == "":
+        chat_name = driver.find_element_by_xpath('//span[@class = "{}"]'.format('iYPsH')).text
+
+    print(chat_name)
+
+    time.sleep(3)
+
+    go_back = driver.find_element_by_xpath('//button[@class = "{}"]'.format('_1aTxu'))
+    go_back.click()
+
     texts = []
     for elem in par_elements:
         texts.append(elem.text)
-    par_msg_box.send_keys("*ChatBot (v1.0):* " + get_answer(texts))
-    btn = driver.find_element_by_class_name('_35EW6')
-    btn.click()
+    answer = get_answer(texts, chat_name)
+    if chat_name in groups_without_authotization:
+        print(answer + '##' + 'without authorization')
+    else:
+        par_msg_box.send_keys("*ChatBot (v1.0):* " + answer)
+        btn = driver.find_element_by_class_name('_35EW6')
+        btn.click()
 
 
+# sets the length of the array from the last scanning of all messages
 def set_length_before():
     # gets all the text elements in a chat
     span_class = 'selectable-text invisible-space copyable-text'
@@ -45,6 +120,7 @@ def set_length_before():
     return len(elements)
 
 
+# checks for a new message in the current chat
 def check_for_current_chat_new_message():
     try:
         # includes the message box where users can enter their message
@@ -59,6 +135,7 @@ def check_for_current_chat_new_message():
         print('No new message')
 
 
+# checks, whether a new messages drops in other chats not in the current chat
 def check_for_new_chat_new_message():
     user = driver.find_element_by_xpath('//span[@class = "{}"]'.format('OUeyt'))
     user.click()
@@ -74,6 +151,7 @@ def check_for_new_chat_new_message():
     send_text(elements, msg_box)
 
 
+# starts the bot
 def start_bot():
     try:
         check_for_new_chat_new_message()
@@ -84,8 +162,9 @@ def start_bot():
 
 if __name__ == '__main__':
     start_bool = input("ChatBot starten [START]: ")
-    if start_bool == "START":
+    if start_bool == "START" or start_bool == "start":
         length_before = 0
+        already_answered = []
         while True:
             start_bot()
             print('Length before attaching: ' + str(length_before))
